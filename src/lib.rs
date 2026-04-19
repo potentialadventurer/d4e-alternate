e84984a2aad84f34ee01046b77eae69c667d111c
potentialadventurer <143354532+potentialadventurer@users.noreply.github.com>
2026-04-19 11:51:49 -0400

    initial draft

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![no_std]
+
+
+mod domain;
+
+#[cfg(test)]
+mod test {
+    use domain;
+
+    use crate::domain::kv_store::Value;
+
+    #[test]
+    fn test_insert() {
+        let mut db = Database::new();
+        let key = Key::parse("key").unwrap();
+        let value = Value::parse("value").unwrap();
+
+        let _ = db.set(key.clone(), value.clone());
+
+        assert_eq!(db.get(&key).unwrap(), value);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+
+//use cortex_m_rt::entry;
+use cortex_m_semihosting::heprintln;
+use panic_halt as _;
+// Print panic message to probe console
+use panic_probe as _;
+
+mod domain;
+
+
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true)]
+mod app {
+    use stm32f4xx_hal::{
+        prelude::*,
+    };
+
+    
+
+    #[shared]
+    struct Shared {}
+
+    // Local resources go here
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        (
+            Shared {
+               // Initialization of shared resources go here
+            },
+            Local {
+                // Initialization of local resources go here
+            },
+            init::Monotonics()
+        )
+    }
+
+    // Optional idle, can be removed if not needed.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            continue;
+        }
+    }
+
+    fn main() {
+        use crate::domain::kv_store::{Database, Key, Value};
+
+        let mut db = Database::new();
+
+        let key = Key::parse("new").unwrap();
+        let value = Value::parse("value").unwrap();
+
+        let _ = db.set(key.clone, value);
+
+        let answer = db.get(&key);
+        hprintln!("value: {:?}", answer);
+
+        let _answer = db.delete(&key);
+    }
+
+}
+
+    
+    
